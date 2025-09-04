17ac0d7ac6f1b093cdb964489b0eee48c14a61b3
Diego Zárate Fernández <dzarate@solopago.com>
2025-09-04 17:11:21 -0600

    feat<keyboard>: Agregando handler para el teclado de la app.

--- lib/src/test/testing_menu.dart
+++ lib/src/test/testing_menu.dart
@@ -1,4 +1,5 @@
 import 'package:demo_pinpad/src/core/utils/debugger/debugger.dart';
+import 'package:demo_pinpad/src/core/utils/keypad/keypad.dart';
 import 'package:demo_pinpad/src/test/card_test.dart';
 import 'package:demo_pinpad/src/test/device_info.dart';
 import 'package:demo_pinpad/src/test/protobuffer_test.dart';
@@ -22,64 +23,81 @@ class _TestingMenuState extends State<TestingMenu> {
     SystemNavigator.pop();
   }
 
+  void _handleDigitPressed(int enteredDigit) async {
+    Debugger.log("Digit: $enteredDigit");
+
+    switch (enteredDigit) {
+      case 1:
+        Navigator.of(context).pushNamed(TestCardReader.route);
+        break;
+      case 2:
+        Navigator.of(context).pushNamed(SerialportTest.route);
+        break;
+      case 3:
+        Navigator.of(context).pushNamed(DeviceInfo.route);
+        break;
+      case 4:
+        Navigator.of(context).pushNamed(ReadProtobufferTest.route);
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return KeyboardListener(
+    return RawKeyboardListener(
       focusNode: _focusNode,
       autofocus: true,
-      onKeyEvent: (KeyEvent event) {
-        Debugger.log("Tecla presionada: ${event.character}");
-      },
+      onKey: rawKeypadHandler(
+        context,
+        onEscape: _closeApp,
+        onDigit: _handleDigitPressed,
+      ),
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Pruebas"),
           actions: [
-            IconButton(onPressed: _closeApp, icon: const Icon(Icons.logout)),
+            IconButton(
+              onPressed: _closeApp,
+              icon: const Icon(Icons.logout),
+            ),
           ],
         ),
-        body: Center(
+        body: Padding(
+          padding: const EdgeInsets.all(4),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  child: Text("1. Lectura de Tarjeta."),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(TestCardReader.route);
-                  },
+              Text(
+                "1. Lectura de Tarjeta.",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
                 ),
               ),
               const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  child: Text("2. Comunicación Serial."),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(SerialportTest.route);
-                  },
+              Text(
+                "2. Comunicación Serial.",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
                 ),
               ),
               const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  child: Text("3. Información del Dispositivo."),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(DeviceInfo.route);
-                  },
+              Text(
+                "3. Información del Dispositivo.",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
                 ),
               ),
               const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  child: Text("4. Recibir Protobuffer."),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(ReadProtobufferTest.route);
-                  },
+              Text(
+                "4. Recibir Protobuffer.",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-              ),
+              )
             ],
           ),
         ),
